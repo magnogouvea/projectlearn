9dca050dbfa07d1b2751daec4f2e4caad33b82da
magnogouvea <magnogouvea@gmail.com>
2023-12-14 19:16:41 -0300

    Subindo o Projeto

---
+++ src/main/java/com/projectlearn/projectlearn/ProjectlearnApplication.java
@@ -0,0 +1,13 @@
+package com.projectlearn.projectlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectlearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectlearn/projectlearn/ProjectlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectlearn.projectlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
